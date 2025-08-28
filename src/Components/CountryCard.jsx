cc365de247cbd2a102f719925b81d5793f55af8c
Rachit <rachitgupta815@gmail.com>
2025-08-28 20:26:57 +0530

    Update

--- src/Components/CountryCard.jsx
+++ src/Components/CountryCard.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { countrydata } from '../Apis/API.jsx'
 
 const CountryCard = () => {
+  const { id } = useParams();
+  const [CountryDetail, setCountryDetail] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await countrydata(id);
+      setCountryDetail(response.data[0]);
+    }
+    fetchData();
+  }, [id]); // added id as dependency
+
+
+  console.log(CountryDetail)
   return (
-    <div>
+    CountryDetail &&
+    <div className='h-[80vh] w-[100%] flex justify-center items-center flex-col lg:flex-row mt-6 mx-auto'>
+      <div className='p-5'>
+        <img src={CountryDetail?.flags?.png} alt={CountryDetail?.flags?.alt} />
+      </div>
+      <div className='text-left p-5'>
+        <h3 className='text-2xl uppercase font-medium text-neutral-600'>
+          {CountryDetail?.name?.common}
+        </h3>
+        <p>Population : {CountryDetail?.population}</p>
+        <p>Region : {CountryDetail?.region}</p>
+        <p>
+          Currency:{" "}
+          {Object.keys(CountryDetail?.currencies || {}).map((code) => {
+            const currency = CountryDetail.currencies[code];
+            return `${currency?.name} (${currency?.symbol})`;
+          }).join(", ")}
+        </p>
+        <p>Continent : {CountryDetail?.continents?.join(", ")}</p>
+        <p>
+          Languages:{" "}
+          {CountryDetail?.languages &&
+            Object.values(CountryDetail.languages).join(", ")}
+        </p>
       
+      <a 
+  href={CountryDetail?.maps?.googleMaps} 
+  target="_blank" 
+  rel="noopener noreferrer"
+>
+  <button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 mt-3">
+Check In Maps  </button>
+</a>
+      
+
+      </div>
     </div>
   )
 }
--- src/Pages/Country.jsx
+++ src/Pages/Country.jsx
@@ -1,36 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import getData from '../Apis/API'
 import { NavLink } from 'react-router-dom'
+import { useNavigation } from 'react-router-dom'
 const Country = () => {
-      const [Countries, setCountries] = useState(null)
-      useEffect(
-        ()=>{
-          const fetchData = async ()=>{
-              const response = await getData();
-              setCountries(response.data);
-          }
-          fetchData()
-        },[]
-      )
+  const [Countries, setCountries] = useState(null)
+  useEffect(
+    () => {
+      const fetchData = async () => {
+        const response = await getData();
+        setCountries(response.data);
+      }
+      fetchData()
+    }, []
+  )
 
 
-      console.log(Countries)
-      
+  console.log(Countries)
+
+     const navigation = useNavigation();
+
+  if (navigation.state === "loading") return <Loader/>
+
+
   return (
 
 
 
-    <div>
-      {Countries && Countries.map(Element=>{
+    <div className='flex flex-wrap justify-center'>
+      {Countries && Countries.map((Element,key) => {
         return (
-          <>
-          <div className='w-70 bg-gradient-to-r from-neutral-800 to-transparent text-white p-5 rounded-lg'>
-          <h3 className='text-xl text-neutral-300'>{Element.name.common}</h3>
-          <p>Population : {Element.population}</p>
-          <p>Language : {Object.keys(Element.languages).map((ele)=>Element.languages[ele] + " ")}</p>
-       <NavLink to={`/detail/${Element.name.common}`}> <button>Know More</button></NavLink>  
-          </div>
-</>
+       
+            <div key={key} className='w-70 bg-gradient-to-r from-neutral-800 to-transparent text-white p-5 rounded-lg m-4'>
+              <img src={Element.flags.png} alt="" className='w-[100%] h-35 object-cover mb-2' />
+              <h3 className='text-xl text-neutral-300'>{Element.name.common}</h3>
+              <p>Population : {Element.population}</p>
+              <p>Language : {Object.keys(Element.languages).map((ele) => Element.languages[ele] + " ")}</p>
+              <NavLink to={`/detail/${Element.name.common}`}> <button className='cursor-pointer px-5 py-2 mt-2 bg-neutral-800 rounded-lg border-1 border-neutral-600'>Know More</button></NavLink>
+            </div>
+
         )
       })}
     </div>
